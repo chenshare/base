ed18a7ef56987c43c5076ee809047f01d99cabc0
weihang12 <763223289@qq.com>
2020-08-21 22:07:04 +0800

    更新配置

---
+++ src/views/category/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>首页</h2>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+export default {
+  name: "Category",
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+
+export default {
+  name: "Home",
+  components:{
+    NavBar
+  },
+  data(){
+    return{
+     banners:[],
+     recommend:[],
+    }
+  },
+  created() {
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners=res.data.banner.list;
+      this.recommend=res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style scoped>
+.home-nav{
+  background-color: #d4237a;
+  color: #f6f6f6;
+}
+</style>
