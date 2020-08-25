317fea167c4ccd47fffeb90b8be9a0ce9ef6bc25
weihang12 <763223289@qq.com>
2020-08-25 23:27:45 +0800

    添加轮播图、物品列表、滑动插件、回到顶部、加载更多功能

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,39 +1,159 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">商品购物街</div></nav-bar>
+    <scroll
+        class="content"
+        ref="scroll"
+        :probe-type="3"
+        @scroll="contentScroll"
+        :pull-up-load="true"
+        @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommend="recommend"></recommend-view>
+      <feature-view></feature-view>
+        <tab-control class="tab-control" :title="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
 <script>
+
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+import FeatureView from "@/views/home/childComps/FeatureView";
+
+
 import NavBar from "@/components/common/navbar/NavBar";
-import {getHomeMultidata} from "@/network/home";
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
+import Scroll from "@/components/common/scroll/Scroll";
+import BackTop from "@/components/content/backtop/BackTop";
+
+
+
+import {getHomeMultidata,getHomeGood} from "@/network/home";
+
 
 export default {
   name: "Home",
   components:{
-    NavBar
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodsList,
+    Scroll,
+    BackTop
   },
   data(){
     return{
      banners:[],
      recommend:[],
+     goods:{
+       'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]},
+      },
+     currentType:'pop',
+     isShowBackTop:false
+
+    }
+  },
+  computed:{
+    showGoods(){
+      return this.goods[this.currentType].list
     }
   },
   created() {
-    getHomeMultidata().then(res=>{
-      console.log(res);
-      this.banners=res.data.banner.list;
-      this.recommend=res.data.recommend.list;
-    })
+    this.getHomeMultidata()
+    this.getHomeGood('pop')
+    this.getHomeGood('new')
+    this.getHomeGood('sell')
+  },
+  methods:{
+    //事件请求相关方法
+    tabClick(index){
+      switch(index){
+        case 0:
+          this.currentType='pop';
+          break
+        case 1:
+          this.currentType='new';
+          break
+        case 2:
+          this.currentType='sell';
+          break
+      }
+    },
+    //网络请求相关方法
+    getHomeMultidata(){
+      getHomeMultidata().then(res=>{
+        this.banners=res.data.banner.list;
+        this.recommend=res.data.recommend.list;
+      })
+    },
+    getHomeGood(type){
+      const page =this.goods[type].page+1
+      getHomeGood(type,page).then(res=>{
+
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page+=1
+        this.$refs.scroll.finishPullUp()
+      })
+    },
+    //回到顶部相关方法
+    backClick(){
+      this.$refs.scroll.scrollTo(0,0)
+    },
+    //监听内容滑动相关方法
+    contentScroll(position){
+      this.isShowBackTop=-position.y>1000
+    },
+    //加载更多相关方法
+    loadMore(){
+      this.getHomeGood(this.currentType)
+      this.$refs.scroll.scroll.refresh()
+    }
+
+
   }
 }
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
 .home-nav{
   background-color: #d4237a;
   color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
+.tab-control{
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+.content{
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+/*.content{*/
+/*  height: calc(100% - 93px);*/
+/*  margin-top: 44px;*/
+
+/*}*/
+
 </style>
